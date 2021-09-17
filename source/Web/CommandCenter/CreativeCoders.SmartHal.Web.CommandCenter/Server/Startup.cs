f96fd37a7f057804053657d8f01cd872b9d9be8d
darthsharp <48331467+darthsharp@users.noreply.github.com>
2021-09-17 22:01:23 +0200

    Disable https for dev

--- source/Web/CommandCenter/CreativeCoders.SmartHal.Web.CommandCenter/Server/Startup.cs
+++ source/Web/CommandCenter/CreativeCoders.SmartHal.Web.CommandCenter/Server/Startup.cs
@@ -36,10 +36,10 @@ namespace CreativeCoders.SmartHal.Web.CommandCenter.Server
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
